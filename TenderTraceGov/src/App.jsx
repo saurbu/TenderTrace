d24f63449d1f15ad8d9b49495cd2f8fea3b47704
saurbu <ss1742@dseu.ac.in>
2026-06-26 04:53:53 +0530

    /

--- TenderTraceGov/src/App.jsx
+++ TenderTraceGov/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import 'remixicon/fonts/remixicon.css'
 
 import Navbar from './components/Navbar'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 import Home from './components/Home'
 import GovernmentLogin from './pages/govt/GovernmentLogin'
